069f920a3262eaa155509db102356760bf9e5205
flyhero <qfwang666@163.com>
2020-06-11 14:02:14 +0800

    feat:修改文章

---
+++ demo/src/main/java/cn/iflyapi/demo/i/plus/Plus.java
@@ -0,0 +1,13 @@
+package cn.iflyapi.demo.i.plus;
+
+/**
+ * @author qfwang
+ * @date 2020-04-22 4:42 PM
+ */
+public class Plus {
+    public static void main(String args[]) {
+        int num = 50;
+        num = num++ * 2;
+        System.out.println(num);
+    }
+}
